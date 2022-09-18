b6c0a5a19c11f4cbfe521afd30538929aa1682cd
Rishmika Sandanu <106467954+ImRishmika@users.noreply.github.com>
2022-09-18 21:33:15 +0530

    Update config.py
    Signed-off-by: Rishmika Sandanu <106467954+ImRishmika@users.noreply.github.com>

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     DATABASE = os.environ.get("DB_URI")
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", 5171347305"").split())
     SUPPORT = os.environ.get("SUPPORT")
